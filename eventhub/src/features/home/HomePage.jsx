a6d66633e4d8dcfa9e059245378a6f32a3a874b9
fera-quzzi <89093224+fera-quzzi@users.noreply.github.com>
2021-08-20 09:24:28 +0100

    End of section 5

---
+++ eventhub/src/features/home/HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Header, Segment, Image, Button, Icon } from 'semantic-ui-react';
+
+export default function HomePage({history}){
+    return(
+        <Segment inverted textAlign="center" vertical className="masthead">
+            <Container>
+                <Header as='h1' inverted>
+                    <Image size="massive" src="/assets/logo.png" style={{ marginBottom: 12 }} />
+                    EventHub
+                </Header>
+
+                <Button onClick={() => history.push('/events')} inverted size="huge">
+                    Get Started
+                    <Icon name="right arrow" inverted />
+                </Button>
+            </Container>
+        </Segment>
+    )
+}
